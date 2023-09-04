e7423471e1c06bb9e46b72636e20e501fe6eb469
Adrian WEAVER <aweaver@student.42.fr>
2023-09-04 15:54:11 +0200

    ADD TIGME_COLOURS:
    Adding tigme pls I <3 APIs script with colours.

---
+++ tigme_colours.sh
@@ -0,0 +1,28 @@
+CR='\e[31m'
+CY='\e[33m'
+NC='\e[0m'
+
+clear;echo -ne "Disclaimer : this is done for \e[34;4mpedalogical\e[0;0m purpose only, \033[31;4mpress F11\033[0;0m (or fn F11 on macs) to quit full screen.\nNothing else was changed on your session this is just a display\n\n\nMy login is \033[33m $USER ! \033[0m\n";
+
+(printf "#   ________  ______   ______         __       __  ________        _______   __        ______\n"  &);
+(printf "#  |        %s|      %s /      %s       |  %s     /  %s|        %s      |       %s |  %s      /      %s \n"  '\'  '\'  '\'  '\'  '\'  '\'  '\'  '\'  '\'  &);
+(printf "#   %sXXXXXXXX %sXXXXXX|  XXXXXX%s      | XX%s   /  XX| XXXXXXXX      | XXXXXXX%s| XX     |  XXXXXX%s\n"  '\'  '\'  '\'  '\'  '\'  '\'  &);
+(printf "#     | XX     | XX  | XX __%sXX      | XXX%s /  XXX| XX__          | XX__/ XX| XX     | XX___%sXX\n"  '\'  '\'  '\'  &);
+(printf "#     | XX     | XX  | XX|    %s      | XXXX%s  XXXX| XX  %s         | XX    XX| XX      %sXX    %s\n"  '\'  '\'  '\'  '\'  &);
+(printf "#     | XX     | XX  | XX %sXXXX      | XX%sXX XX XX| XXXXX         | XXXXXXX | XX      _%sXXXXXX%s\n"  '\'  '\'  '\'  &);
+(printf "#     | XX    _| XX_ | XX__| XX      | XX %sXXX| XX| XX_____       | XX      | XX_____|  %s__| XX\n"  '\'  '\'  &);
+(printf "#     | XX   |   XX %s %sXX    XX      | XX  %sX | XX| XX     %s      | XX      | XX     %sXX    XX\n"  '\'  '\'  '\'  '\'  '\'  &);
+(printf "#      %sXX    %sXXXXXX  %sXXXXXX        %sXX      %sXX %sXXXXXXXX       %sXX       %sXXXXXXXX %sXXXXXX\n"  '\'  '\'  '\'  '\'  '\'  '\'  '\'  '\'  '\'  &);
+(printf "# \n" &);
+(printf "#   ______           __   ______          ______   _______  ______\n"  &);
+(printf "#  |      %s         /  %s /      %s        /      %s |       %s|      %s\n"  '\'  '\'  '\'  '\'  '\'  '\'  &);
+(printf "#   %sXXXXXX        /  ${CR}XX${NC}|  ${CR}XXXXXX${NC}%s      |  ${CY}XXXXXX${NC}%s| ${CY}XXXXXXX${NC}%s${CY}XXXXXX${NC}  _______\n"  '\'  '\'  '\'   '\\'  &);
+(printf "#    | XX         /  ${CR}XX${NC}  %s${CR}XX${NC}__| ${CR}XX${NC}      | ${CY}XX${NC}__| ${CY}XX${NC}| ${CY}XX${NC}__/ ${CY}XX${NC} | ${CY}XX${NC}   /       %s\n"  '\'  '\'  &);
+(printf "#    | XX        |  ${CR}XX${NC}    |     ${CR}XX${NC}      | ${CY}XX    XX${NC}| ${CY}XX    XX${NC} | ${CY}XX${NC}  |  ${CY}XXXXXXX${NC}\n"  &);
+(printf "#    | XX         %s${CR}XX${NC}%s   __%s${CR}XXXXX${NC}%s      | ${CY}XXXXXXXX${NC}| ${CY}XXXXXXX${NC}  | ${CY}XX${NC}   %s${CY}XX${NC}    %s\n"  '\'  '\'  '\'  '\'  '\'  '\'  &);
+(printf "#   _| XX_         %s${CR}XX${NC}%s |  %s__| ${CR}XX${NC}      | ${CY}XX${NC}  | ${CY}XX${NC}| ${CY}XX${NC}      _| ${CY}XX${NC}_  _%s${CY}XXXXXX${NC}%s\n"  '\'  '\'  '\'  '\'  '\'  &);
+(printf "#  |   XX %s         %s${CR}XX${NC}%s %s${CR}XX${NC}    ${CR}XX${NC}      | ${CY}XX${NC}  | ${CY}XX${NC}| ${CY}XX${NC}     |   ${CY}XX${NC} %s|       ${CY}XX${NC}\n"  '\'  '\'  '\'  '\'  '\'  &);
+(printf "#   %sXXXXXX          %s${CR}XX${NC}  %s${CR}XXXXXX${NC}        %s${CY}XX${NC}   %s${CY}XX${NC} %s${CY}XX${NC}      %s${CY}XXXXXX${NC} %s${CY}XXXXXXX${NC}\n"  '\'  '\'  '\'  '\'  '\'  '\'  '\'  '\'  &);
+(printf "#\n" &);
+(printf "#\n" &);
+(printf "#\n" &);
